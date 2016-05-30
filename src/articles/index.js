5e8d6dd88264743a01d5cdbe8b563a4bb8b73ec5
reco <recotj@gmail.com>
2016-05-30 15:33:21 +0800

    add articles

--- src/articles/index.js
+++ src/articles/index.js
@@ -1,7 +1,9 @@
 define(function (require, module, exports) {
 	var articles = [
 		require('yaml!./article_1.yml'),
-		require('yaml!./article_2.yml')
+		require('yaml!./article_2.yml'),
+		require('yaml!./article_3.yml'),
+		require('yaml!./article_4.yml')
 	];
 	var template = require('hbs!./article');
 
